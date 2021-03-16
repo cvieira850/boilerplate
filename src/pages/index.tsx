2413ca1bf5cbd052fcb4f4b0eef4b80509dc2311
Caio Vieira <contato@caiovieira.com.br>
2021-03-16 20:47:30 -0300

    chore: add eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import Head from 'next/head'
 
 type Props = {
-  title: string 
+  title: string
 }
 
-export default function Home({title = 'Caio'} : Props) {
+export default function Home({ title = 'Caio' }: Props) {
   return (
     <div >
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1>Hello Caio</h1>
+      <h1>Hello Caio</h1>
     </div>
   )
 }

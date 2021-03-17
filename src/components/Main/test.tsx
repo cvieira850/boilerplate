824fac862cd5e81bb246ca3a1031512984a0ca12
Caio Vieira <contato@caiovieira.com.br>
2021-03-17 12:02:24 -0300

    test: create first component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,9 +6,16 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançado - boilerplate/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('Should render the colors correctly', () => {
+    const { container } = render(<Main />)
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
   })
 })

462135042c5202aaf7082001058ac03a79050b84
Caio Vieira <contato@caiovieira.com.br>
2021-03-17 13:54:34 -0300

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/components/**/stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs/register"
+  ]
+}

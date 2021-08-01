96ca6449b88953ec5875c66394c1fed49eca108b
Caio Vieira <contato@caiovieira.com.br>
2021-08-01 17:12:43 -0300

    feat: add theme and adjust generator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,16 @@
-import GlobalStyles from '../src/styles/global'
-
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles/>
       <Story/>
-    </>
+    </ThemeProvider>
   )
 ]
 
